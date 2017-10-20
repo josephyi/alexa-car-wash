62ec525234c95e5d50e17d63e2f24a5df006b691
Joseph Yi <joe@josephyi.com>
2017-10-20 02:29:10 -0700

    randomize speechcons, include city and state in response

--- src/Handlers.js
+++ src/Handlers.js
@@ -9,15 +9,51 @@ const LOCATION_PERMISSIONS = [COUNTRY_AND_POSTAL_CODE_PERMISSION];
 const NEED_PERMISSION_MESSAGE =
   "To automatically use your device location, please enable Location permissions in the Amazon Alexa app. You can also use a city and state. For example, launch by saying, Ask Car Wash in San Diego, California.";
 
-const darkskyResponse = async function(address) {
-  const googleMapsResponse = await geocode({ address });
-  const { lat, lng } = googleMapsResponse.results[0].geometry.location;
-  return await forecast(process.env.DARKSKY_API_KEY, lat, lng);
+const HAPPY_SPEECHCONS = [
+  "Booya",
+  "Dynomite",
+  "Hip hip hooray",
+  "Hurrah",
+  "Hurray",
+  "Huzzah",
+  "Oh dear.  Just kidding.  Hurray",
+  "Kaboom",
+  "Kaching",
+  "Phew",
+  "Whee",
+  "Woo hoo",
+  "Yay"
+];
+const SAD_SPEECHCONS = [
+  "Argh",
+  "Aw man",
+  "Blarg",
+  "Blast",
+  "Boo",
+  "Bummer",
+  "Darn",
+  "D'oh",
+  "Dun dun dun",
+  "Eek",
+  "Le sigh",
+  "Mamma mia",
+  "Oh boy",
+  "Oh dear",
+  "Oof",
+  "Ouch",
+  "Ruh roh",
+  "Shucks",
+  "Uh oh",
+  "Wah wah",
+  "Yikes"
+];
+
+const random = array => {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
-const sayForRainyDays = (timezone, rainyDayTimes) => {
+const sayForRainyDays = (city, state, timezone, rainyDayTimes) => {
   const days = rainyDayTimes.reduce((accumulator, item, idx, arr) => {
-
     const day = moment(item * 1000)
       .tz(timezone)
       .startOf("day")
@@ -29,12 +65,16 @@ const sayForRainyDays = (timezone, rainyDayTimes) => {
       });
 
     // humanized day and ', ' and ' and ' as needed
-    const output = `${day}${idx < arr.length - 1 && arr.length > 2 ? ', ': ''}${arr.length > 1 && idx + 1 === arr.length - 1 ? ' and ': ''}`
+    const output = `${day}${idx < arr.length - 1 && arr.length > 2
+      ? ", "
+      : ""}${arr.length > 1 && idx + 1 === arr.length - 1 ? " and " : ""}`;
 
     return accumulator.concat(output);
-  }, '');
+  }, "");
 
-  return `<say-as interpret-as="interjection">boo!</say-as> It's going to rain ${days}, so you might not want to wash your car. Check again tomorrow to see if conditions improve.`;
+  return `<say-as interpret-as="interjection">${random(
+    SAD_SPEECHCONS
+  )}!</say-as> In ${city}, ${state}, it's going to rain ${days}, so you might not want to wash your car. Check again tomorrow to see if conditions improve.`;
 };
 
 const unhandledHandler = function() {
@@ -71,8 +111,20 @@ const carWashCheckHandler = async function() {
 
   if (address != null) {
     try {
-      console.log('Address - ' + address);
-      const weatherResponse = await darkskyResponse(address);
+      const googleMapsResponse = await geocode({ address });
+      const { lat, lng } = googleMapsResponse.results[0].geometry.location;
+
+      let city, state;
+      googleMapsResponse.results[0].address_components.forEach(e => {
+        if (e.types[0] === "locality") city = e.long_name;
+        if (e.types[0] === "administrative_area_level_1") state = e.long_name;
+      });
+
+      const weatherResponse = await forecast(
+        process.env.DARKSKY_API_KEY,
+        lat,
+        lng
+      );
       const timezone = weatherResponse["timezone"];
       const forecastWeek = weatherResponse["daily"]["data"];
       const rainyDayTimes = forecastWeek
@@ -83,11 +135,16 @@ const carWashCheckHandler = async function() {
         case 0:
           this.emit(
             ":tell",
-            `<say-as interpret-as="interjection">huzzah!</say-as> Today is a good day to wash your car because there is no rain in the 7 day forecast.`
+            `<say-as interpret-as="interjection">${random(
+              HAPPY_SPEECHCONS
+            )}!</say-as>, In ${city}, ${state}, it's a good day to wash your car because there is no rain in the 7 day forecast.`
           );
           break;
         default:
-          this.emit(":tell", sayForRainyDays(timezone, rainyDayTimes));
+          this.emit(
+            ":tell",
+            sayForRainyDays(city, state, timezone, rainyDayTimes)
+          );
           break;
       }
     } catch (error) {
